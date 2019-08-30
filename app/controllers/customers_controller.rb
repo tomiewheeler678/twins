f8b4bb7d52341689dd7a3bef4038e15f08dc98c9
tomiewheeler678 <tomiewheeler678@gmail.com>
2019-08-30 22:25:23 +0000

    simpleform

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -69,6 +69,6 @@ class CustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params.fetch(:customer, {})
+      params.require(:customer).permit(:first_name, :middle_initial, :last_name, :date_of_birth, :address, :city, :state, :zip_code, :email, :phone_number)
     end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -69,6 +69,6 @@ class PetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pet_params
-      params.fetch(:pet, {})
+      params.require(:pet).permit(:petName, :petSpecies, :petBreed, :weight, :age, :color, :favortieToy)
     end
 end
